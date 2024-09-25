e5c282240401e7e1f42216b297e6ebd141d1baff
bsh <dnfg12@naver.com>
2024-09-25 13:36:38 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// 페이지 컴포넌트
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
